e59910751b3d757175920ced333620944599a764
Pablo Carranza <pcarranza@gmail.com>
2014-04-23 22:41:34 +0100

    basic cipher and decripher

--- encrypt.rb
+++ encrypt.rb
@@ -1,17 +1,78 @@
+require "openssl"
 
-class Encrypter
+MEGABYTE=1024*1024
+
+class CryptoHelper
 
     attr_reader :key
 
-    def initialize(key)
-        raise ArgumentError.new "Key cannot be nil" if key.nil?
-        raise ArgumentError.new "Key must be public" unless key.public?
-        @key = key
+    def initialize(keyfile)
+        raise ArgumentError, "Key cannot be nil" if keyfile.nil?
+        @rsakey = OpenSSL::PKey::RSA::new File.new(keyfile)
+        cipher = OpenSSL::Cipher::AES256.new 'CBC'
+        @key = cipher.random_key
+        @iv = cipher.random_iv
     end
 
-    def encrypt(text)
-        raise ArgumentError.new "Invalid text" if text.nil? or text.empty?
-        key.public_encrypt text
+    def encrypt_text(text)
+        raise ArgumentError, "Invalid text" if text.nil? or text.empty?
+        @rsakey.public_encrypt text
+    end
+
+    def encrypt_file(source, target)
+        source = open source, 'r'
+        target = open target, 'w'
+        @shared_key = OpenSSL::Cipher::AES256.new 'CBC'
+        @shared_key.encrypt
+        @shared_key.key = @key
+        @shared_key.iv = @iv
+
+        begin
+            source.each_chunk do | chunk |
+                target.write @shared_key.update(chunk)
+            end
+            target.write @shared_key.final
+        ensure
+            target.close
+            source.close
+        end
+    end
+
+    def decrypt_file(source, target)
+        source = open source, 'r'
+        target = open target, 'w'
+        @shared_key = OpenSSL::Cipher::AES256.new 'CBC'
+        @shared_key.decrypt
+        @shared_key.key = @key
+        @shared_key.iv = @iv
+        begin
+            source.each_chunk do | chunk |
+                target.write @shared_key.update(chunk)
+            end
+            target.write @shared_key.final
+        ensure
+            target.close
+            source.close
+        end
+    end
+
+    def open file, mode
+        case file
+        when String 
+            File.open file, mode
+        when File
+            file
+        else
+            raise ArgumentError, 'Invalid file'
+        end
     end
 
 end
+
+
+class File
+    def each_chunk(chunk_size=MEGABYTE)
+        yield read(chunk_size) until eof?
+    end
+end
+
--- spec/encrypt_spec.rb
+++ spec/encrypt_spec.rb
@@ -1,38 +1,77 @@
 require "openssl"
+require "tempfile"
+require "securerandom"
+require "digest/md5"
 require_relative '../encrypt.rb'
 
-describe Encrypter do
+describe CryptoHelper do
     it "raises ArgumentError without public key" do
         expect do 
-            Encrypter.new nil 
+            CryptoHelper.new nil 
         end.to raise_error(ArgumentError)
     end
 
     context "with a keypair" do
         keypair = OpenSSL::PKey::RSA::new 1024
-        encrypter = Encrypter.new keypair
+        keyfile = Tempfile.new('keypair')
+        keyfile.write(keypair.to_pem)
+        keyfile.write(keypair.public_key.to_pem)
+        keyfile.close
+        crypto = CryptoHelper.new keyfile.path
 
         it "contains the key" do
-            encrypter.key.should_not.nil?
+            crypto.key.should_not.nil?
         end
         it "raises ArgumentError encrypting empty text" do
             expect do
-                encrypter.encrypt ""
+                crypto.encrypt_text ""
             end.to raise_error(ArgumentError)
         end
         it "raises ArgumentError encrypting nil text" do
             expect do
-                encrypter.encrypt nil 
+                crypto.encrypt_text nil 
             end.to raise_error(ArgumentError)
         end
         it "encrypts a text smaller than the key" do
-            crypto = encrypter.encrypt "some text"
-            crypto.should_not.nil?
+            encrypted = crypto.encrypt_text "some text"
+            encrypted.should_not.nil?
         end
         it "fails to encrypt text bigger than the key" do
             expect do
-                encrypter.encrypt "a" * 2048
+                crypto.encrypt_text "a" * 2048
             end.to raise_error(OpenSSL::PKey::RSAError)
         end
+
+        context "with a file bigger than the key" do
+            bigfile = Tempfile.new('bigfile')
+            bigfile.write(SecureRandom.hex) until bigfile.length > 1024
+            bigfile.close
+            source = bigfile.path
+            source_md5 = Digest::MD5.digest File.read(source)
+
+            it "encrypts and then decrypts the file" do
+                source = bigfile.path
+                target = Dir::Tmpname::make_tmpname 'encrypted_big_file', nil
+                decrypted = Dir::Tmpname::make_tmpname 'decrypted_big_file', nil
+
+                begin
+                    crypto.encrypt_file(source, target)
+                    encrypted_md5 = Digest::MD5.digest File.read(target)
+                    source_md5.should_not eq encrypted_md5
+
+                    crypto.decrypt_file(target, decrypted)
+                    decrypted_md5 = Digest::MD5.digest File.read(decrypted)
+                    decrypted_md5.should eq source_md5
+                ensure
+                    File.delete target
+                    File.delete decrypted
+                end
+            end
+
+        end
+
+        keyfile.unlink
     end
 end
+
+
