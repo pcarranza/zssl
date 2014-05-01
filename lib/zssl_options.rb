779816f4865ecf8c53a41d72aeb58acc70e22e7d
Pablo Carranza <pcarranza@gmail.com>
2014-05-01 17:24:32 +0100

    Understands ssh public keys, and some improvements in file handling

--- lib/zssl_options.rb
+++ lib/zssl_options.rb
@@ -9,9 +9,11 @@ module Zoocial
         def initialize
             @options = {}
             @parser = OptionParser.new do |opts|
-                opts.banner = "usage: zssl [options] MODE SOURCE [TARGET]"
+                opts.banner = "usage: zssl [options] MODE [SOURCE] [TARGET]"
                 opts.separator ""
                 opts.separator "MODE: e[ncrypt], using a public key or d[ecrypt] using a private key"
+                opts.separator "SOURCE: a file that exists, if it not declared stdin will be used"
+                opts.separator "TARGET: a file that can exist or not, if it not declared stdout will be used"
                 opts.separator ""
                 opts.on "-i", "--identity [FILE]", "Key identity file to encrypt or decrypt, DER or PEM encoded, by default it is your ssh key (~/.ssh/id_rsa)" do |v|
                     @options[:key] = v
@@ -51,24 +53,21 @@ module Zoocial
                     if filename.nil?
                         default
                     else
+                        raise ArgumentError, "File #{filename} could not be found" unless File.exists? filename or mode == 'w'
                         File.open filename, mode
                     end
                 end
 
-                unless @options.has_key? :key
-                    Dir.glob(File.expand_path('~/.ssh/id_?sa')).each do |f|
-                        @options[:key] = f
-                    end
-                    raise ArgumentError, "No ssh identity found" unless @options.has_key? :key
-                end
+                @options[:key] = File.expand_path('~/.ssh/id_rsa') unless @options.has_key? :key
+                raise ArgumentError, "No RSA key provided" unless @options.has_key? :key
 
                 @source = open_file.call @source, 'r', $stdin
                 @target = open_file.call @target, 'w', $stdout
                 @key = open_file.call @options[:key], 'r'
-            rescue
-                print_error
-                exit 1
+                rescue
+                    print_error
+                    exit 1
+                end
             end
         end
     end
-end
