e2739148f86bf789cfaf95ebb5521c897f6cf706
Pablo Carranza <pcarranza@gmail.com>
2014-08-10 12:03:58 +0100

    Some rspec cleanup

--- spec/zssl_spec.rb
+++ spec/zssl_spec.rb
@@ -5,120 +5,120 @@ require "digest/md5"
 
 describe Zoocial::Cipher do
 
-    it "errs without public key" do
-        expect do 
-            Zoocial::Cipher.new nil 
-        end.to raise_error 'Key cannot be nil'
+  it "errs without public key" do
+    expect do
+      Zoocial::Cipher.new nil
+    end.to raise_error 'Key cannot be nil'
+  end
+
+  it "errs if key is not valid" do
+    expect do
+      Zoocial::Cipher.new 123
+    end.to raise_error 'Unsupported key 123'
+  end
+
+  key_size = 1024
+
+  context "with a source file" do
+    delete = Proc.new do |file|
+      case file
+      when String
+        filename = file
+      when File, IO
+        filename = file.path
+      end
+      File.delete filename if File.exist? filename
     end
 
-    it "errs if key is not valid" do
+    bigfile = Tempfile.new('bigfile')
+    bigfile.write(SecureRandom.hex) until bigfile.length > key_size * 16
+    bigfile.close
+    source = bigfile.path
+    source_md5 = Digest::MD5.digest File.read(source)
+
+    context "with a DSA keypair" do
+      keypair = OpenSSL::PKey::DSA::new 1024
+
+      it "fails to build" do
         expect do
-            Zoocial::Cipher.new 123
-        end.to raise_error 'Unsupported key 123'
+          Zoocial::Cipher.new keypair
+        end.to raise_error 'DSA is not supported'
+      end
     end
 
-    key_size = 1024
+    context "with an ssh public and private key file" do
+      pubkeyfile = File.join(File.dirname(__FILE__), 'id_rsa_test.pub')
+      privkeyfile = File.join(File.dirname(__FILE__), 'id_rsa_test')
 
-    context "with a source file" do
-        delete = Proc.new do |file|
-            case file
-            when String
-                filename = file
-            when File, IO
-                filename = file.path
-            end
-            File.delete filename if File.exist? filename
+      it "reads the key if it is an already open file" do
+        File.open pubkeyfile, 'r' do |f|
+          crypto = Zoocial::Cipher.new f
+          crypto.pkey.should_not.nil?
         end
-        
-        bigfile = Tempfile.new('bigfile')
-        bigfile.write(SecureRandom.hex) until bigfile.length > key_size * 16
-        bigfile.close
+      end
+
+      it "contains a valid key" do
+        crypto = Zoocial::Cipher.new pubkeyfile
+        crypto.pkey.should_not.nil?
+      end
+
+      it "encrypts and then decrypts a file correctly" do
+        encrypter = Zoocial::Cipher.new pubkeyfile
+        decrypter = Zoocial::Cipher.new privkeyfile
         source = bigfile.path
-        source_md5 = Digest::MD5.digest File.read(source)
+        encrypted = Dir::Tmpname::make_tmpname 'encrypted_big_file', nil
+        decrypted = Dir::Tmpname::make_tmpname 'decrypted_big_file', nil
 
-        context "with a DSA keypair" do
-            keypair = OpenSSL::PKey::DSA::new 1024
+        begin
+          encrypter.send :encrypt, source, encrypted
+          encrypted_md5 = Digest::MD5.digest File.read(encrypted)
+          source_md5.should_not eq encrypted_md5
 
-            it "fails to build" do
-                expect do 
-                    Zoocial::Cipher.new keypair
-                end.to raise_error 'DSA is not supported'
-            end
+          decrypter.decrypt(encrypted, decrypted)
+          decrypted_md5 = Digest::MD5.digest File.read(decrypted)
+          decrypted_md5.should eq source_md5
+        ensure
+          delete.call encrypted
+          delete.call decrypted
         end
 
-        context "with an ssh public and private key file" do
-            pubkeyfile = File.join(File.dirname(__FILE__), 'id_rsa_test.pub')
-            privkeyfile = File.join(File.dirname(__FILE__), 'id_rsa_test')
-
-            it "reads the key if it is an already open file" do
-                File.open pubkeyfile, 'r' do |f|
-                    crypto = Zoocial::Cipher.new f
-                    crypto.pkey.should_not.nil?
-                end
-            end
-
-            it "contains a valid key" do
-                crypto = Zoocial::Cipher.new pubkeyfile
-                crypto.pkey.should_not.nil?
-            end
-
-            it "encrypts and then decrypts a file correctly" do
-                encrypter = Zoocial::Cipher.new pubkeyfile
-                decrypter = Zoocial::Cipher.new privkeyfile
-                source = bigfile.path
-                encrypted = Dir::Tmpname::make_tmpname 'encrypted_big_file', nil
-                decrypted = Dir::Tmpname::make_tmpname 'decrypted_big_file', nil
-
-                begin
-                    encrypter.send :encrypt, source, encrypted
-                    encrypted_md5 = Digest::MD5.digest File.read(encrypted)
-                    source_md5.should_not eq encrypted_md5
-
-                    decrypter.decrypt(encrypted, decrypted)
-                    decrypted_md5 = Digest::MD5.digest File.read(decrypted)
-                    decrypted_md5.should eq source_md5
-                ensure
-                    delete.call encrypted
-                    delete.call decrypted
-                end
-
-            end
-        end
-
-        context "with a generated RSA keypair" do
-            keypair = OpenSSL::PKey::RSA::new key_size
-
-            it "contains the key" do
-                crypto = Zoocial::Cipher.new keypair
-                crypto.pkey.should eq keypair
-            end
-
-            context "written to a temporary file" do
-                keyfile = Tempfile.new('keypair')
-                keyfile.write(keypair.to_pem)
-                keyfile.write(keypair.public_key.to_pem)
-                keyfile.close
-                crypto = Zoocial::Cipher.new keyfile.path
-
-                it "encrypts and then decrypts a file correctly" do
-                    source = bigfile.path
-                    encrypted = Dir::Tmpname::make_tmpname 'encrypted_big_file', nil
-                    decrypted = File.open(Dir::Tmpname::make_tmpname('decrypted_big_file', nil), 'w')
-
-                    begin
-                        crypto.send :encrypt, source, encrypted
-                        encrypted_md5 = Digest::MD5.digest File.read(encrypted)
-                        source_md5.should_not eq encrypted_md5
-
-                        crypto.decrypt(encrypted, decrypted)
-                        decrypted_md5 = Digest::MD5.digest File.read(decrypted)
-                        decrypted_md5.should eq source_md5
-                    ensure
-                        delete.call encrypted
-                        delete.call decrypted
-                    end
-                end
-            end
-        end
+      end
     end
+
+    context "with a generated RSA keypair" do
+      keypair = OpenSSL::PKey::RSA::new key_size
+
+      it "contains the key" do
+        crypto = Zoocial::Cipher.new keypair
+        crypto.pkey.should eq keypair
+      end
+
+      context "written to a temporary file" do
+        keyfile = Tempfile.new('keypair')
+        keyfile.write(keypair.to_pem)
+        keyfile.write(keypair.public_key.to_pem)
+        keyfile.close
+        crypto = Zoocial::Cipher.new keyfile.path
+
+        it "encrypts and then decrypts a file correctly" do
+          source = bigfile.path
+          encrypted = Dir::Tmpname::make_tmpname 'encrypted_big_file', nil
+          decrypted = File.open(Dir::Tmpname::make_tmpname('decrypted_big_file', nil), 'w')
+
+          begin
+            crypto.send :encrypt, source, encrypted
+            encrypted_md5 = Digest::MD5.digest File.read(encrypted)
+            source_md5.should_not eq encrypted_md5
+
+            crypto.decrypt(encrypted, decrypted)
+            decrypted_md5 = Digest::MD5.digest File.read(decrypted)
+            decrypted_md5.should eq source_md5
+          ensure
+            delete.call encrypted
+            delete.call decrypted
+          end
+        end
+      end
+    end
+  end
 end
